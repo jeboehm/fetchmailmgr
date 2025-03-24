3d352b86e361846e40652359ba0c20a3c38373a8
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-03-24 15:49:46 +0100

    chore: lint code, update documentation

--- src/redisconnection.js
+++ src/redisconnection.js
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redisUrl = process.env.REDIS_URL;
+const redisUrl = process.env.REDIS_URL || "redis://localhost:6379";
 
 const client = createClient({
   url: redisUrl,
