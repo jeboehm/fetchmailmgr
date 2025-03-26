33b68c1456198539d4d51ac98d12bd53ba634272
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-03-26 01:44:26 +0100

    feat: allow redis configuration via REDIS_(HOST|PORT|PASSWORD|DB) env variables

--- src/redisconnection.js
+++ src/redisconnection.js
@@ -1,8 +1,36 @@
 import { createClient } from 'redis';
 
+const buildDsn = (config) => {
+  const { host, port, password, db } = config;
+
+  let dsn = `redis://${host}:${port}`;
+
+  if (password) {
+    dsn = `redis://:${password}@${host}:${port}`;
+  }
+
+  if (db) {
+    dsn = `${dsn}/${db}`;
+  }
+
+  return dsn;
+};
+
+let redisUrl;
+
+if (process.env.REDIS_HOST && process.env.REDIS_PORT) {
+  redisUrl = buildDsn({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+    db: process.env.REDIS_DB,
+  });
+} else {
+  redisUrl = process.env.REDIS_URL;
+}
+
 const client = createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
-  password: process.env.REDIS_PASSWORD || undefined,
+  url: redisUrl,
 });
 
 client.on('error', (error) => {
