1e6ea267bd93be3311fcc77746a9d3dee7eb0ae2
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-04-05 10:32:13 +0200

    fix: typo in sslcertck

--- src/config.js
+++ src/config.js
@@ -20,7 +20,7 @@ const template = (account) => {
   if (account.verifySsl) {
     options.push('sslcertck')
   } else {
-    options.push('nosslcertck')
+    options.push('no sslcertck')
   }
 
   return `
