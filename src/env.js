8847834371805b304ba46c40b8f983404c6eac6b
Jeffrey Böhm <hello@jeffrey-boehm.de>
2025-10-17 15:19:47 +0200

    feat: change MTA_HOST to FETCHMAIL_SMTP_ADDRESS
    BREAKING CHANGE
    You need to replace MTA_HOST with FETCHMAIL_SMTP_ADDRESS

--- src/env.js
+++ src/env.js
@@ -5,5 +5,7 @@ export const REDIS_DB = process.env.REDIS_DB
 export const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 export const FETCHMAIL_PATH = process.env.FETCHMAIL_PATH || '/usr/bin/fetchmail'
 export const TEMP_DIR = process.env.TEMP_DIR || '/tmp'
-export const MTA_HOST = process.env.MTA_HOST || 'localhost'
+export const FETCHMAIL_SMTP_ADDRESS = process.env.FETCHMAIL_SMTP_ADDRESS?.replace(/:/g, '/') || (() => {
+  throw new Error('FETCHMAIL_SMTP_ADDRESS environment variable is required')
+})()
 export const DEBUG = process.env.DEBUG
